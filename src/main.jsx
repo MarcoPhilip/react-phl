a1a285985128368592e0eaee6aa188874355cedc
marcophilip <marcophilipagapito@gmail.com>
2026-02-08 20:39:20 -0800

    style: base app layout and typography

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
